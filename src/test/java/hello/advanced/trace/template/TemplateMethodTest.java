d29ff219b6c44c7e746ee1465e3bf0052925a15f
seonggon do <62453668+mooh2jj@users.noreply.github.com>
2022-02-03 14:05:28 +0900

    feat: TemplateMethodTest 구현 - 익명 내부 클래스 적용

--- src/test/java/hello/advanced/trace/template/TemplateMethodTest.java
+++ src/test/java/hello/advanced/trace/template/TemplateMethodTest.java
@@ -41,4 +41,28 @@ public class TemplateMethodTest {
         AbstractTemplate template2 = new SubClassLogic2();
         template2.execute();
     }
+
+    /**
+     * 템플릿 메서드 패턴 적용 : 익명 내부 클래스 적용
+     */
+    @Test
+    void templateMethodV2() {
+        AbstractTemplate template1 = new AbstractTemplate() {
+            @Override
+            protected void call() {
+                log.info("비즈니스 로직1 실행");
+            }
+        };
+        log.info("비즈니스 로직1 실행");
+        template1.execute();
+
+        AbstractTemplate template2 = new AbstractTemplate() {
+            @Override
+            protected void call() {
+                log.info("비즈니스 로직2 실행");
+            }
+        };
+        log.info("비즈니스 로직2 실행");
+        template2.execute();
+    }
 }
